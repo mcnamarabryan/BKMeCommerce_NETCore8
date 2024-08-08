8fbee99719574c65ff254ba9275f278c5d90dc7a
Bryan McNamara <bryan@bkm-tech.com>
2024-08-08 16:50:12 -0400

    Create MVC and add ApplicationDbContext, connect to server and create db

--- BKMeCommerceWeb/Program.cs
+++ BKMeCommerceWeb/Program.cs
@@ -1,7 +1,13 @@
+using BKMeCommerceWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(o => 
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
