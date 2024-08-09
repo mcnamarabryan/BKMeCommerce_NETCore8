fd4e3a43755163e58c43a47f547e9be5705de455
Bryan McNamara <bryan@bkm-tech.com>
2024-08-09 15:56:08 -0400

    Implement category CRUD controllers and views

--- BKMeCommerceWeb/Controllers/CategoryController.cs
+++ BKMeCommerceWeb/Controllers/CategoryController.cs
@@ -1,12 +1,90 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BKMeCommerceWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using BKMeCommerceWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BKMeCommerceWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //if (category.Name == category.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "Category Name can not be the same as the Display Order");
+            //}
+            //if (category.Name == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? categoryId)
+        {
+            if (categoryId == 0 || categoryId == null) { return NotFound(); }
+            Category? categoryFromDb = _context.Categories.Find(categoryId);
+            if (categoryFromDb == null) { return NotFound(); }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? categoryId)
+        {
+            if (categoryId == 0 || categoryId == null) { return NotFound(); }
+            Category? categoryFromDb = _context.Categories.Find(categoryId);
+            if (categoryFromDb == null) { return NotFound(); }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? categoryId)
+        {
+            if (categoryId == 0 || categoryId == null) { return NotFound(); }
+            Category? category = _context.Categories.Find(categoryId);
+            if (category == null) { return NotFound(); }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Category");
         }
     }
 }
