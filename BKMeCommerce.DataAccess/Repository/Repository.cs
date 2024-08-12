132ee7e367662207dc5104a3bfa694d81d832caa
Bryan McNamara <bryan@bkm-tech.com>
2024-08-12 16:41:32 -0400

    Implement generic repository

---
+++ BKMeCommerce.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BKMeCommerce.DataAccess.Data;
+using BKMeCommerce.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace BKMeCommerce.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+
+        void IRepository<T>.Add(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        T IRepository<T>.GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        void IRepository<T>.Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        void IRepository<T>.RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
