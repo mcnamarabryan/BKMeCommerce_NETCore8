e1a5f6013ca13d72735ce25961506ce55c70ebfc
Bryan McNamara <bryan@bkm-tech.com>
2024-08-16 18:13:51 -0400

    Company, product and shoppingcart models

---
+++ BKMeCommerce.Models/Product.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKMeCommerce.Models
+{
+    public  class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [ValidateNever]
+        public int CategoryId { get; set; }
+
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public decimal ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public decimal Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public decimal Price100 { get; set; }
+    }
+}
