488fade66490f9a1bd795c87c35b2025491b3e2a
Bryan McNamara <bryan@bkm-tech.com>
2024-08-21 15:31:30 -0400

    Improve product details controller/action and view

--- BKMeCommerceWeb/Areas/Customer/Controllers/HomeController.cs
+++ BKMeCommerceWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using BKMeCommerce.DataAccess.Repository.IRepository;
 using BKMeCommerce.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BKMeCommerceWeb.Areas.Customer.Controllers
 {
@@ -23,14 +25,45 @@ namespace BKMeCommerceWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
-        public IActionResult Details(int? id)
+        public IActionResult Details(int? productId)
         {
-            if (id == null || id == 0)
+            if (productId == null || productId == 0)
             {
                 return NotFound();
             }
-            Product product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties: "Category");
-            return View(product);
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            ShoppingCart cart = new()
+            {
+                Product = product,
+                ProductId = product.Id,
+                Count = 1,
+            };
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart cart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            cart.ApplicationUserId = userId;
+
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == cart.ProductId);
+            if (cartFromDb != null)
+            {
+                cartFromDb.Count += cart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Add(cart);
+            }
+
+
+            _unitOfWork.Save();
+            TempData["success"] = "Shopping cart successfully updated";
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
