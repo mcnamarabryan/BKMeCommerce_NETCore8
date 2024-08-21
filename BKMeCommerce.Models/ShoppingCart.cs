44f8af607aaeda887b96ab2b4060629721a2eddb
Bryan McNamara <bryan@bkm-tech.com>
2024-08-21 15:30:12 -0400

    Implement shopping cart

--- BKMeCommerce.Models/ShoppingCart.cs
+++ BKMeCommerce.Models/ShoppingCart.cs
@@ -13,12 +13,14 @@ namespace BKMeCommerce.Models
     public class ShoppingCart
     {
         [Key]
+        [ValidateNever]
         public int Id { get; set; }
 
-        public int ProductId { get; set; }
         [ForeignKey("ProductId")]
         [ValidateNever]
         public Product Product { get; set; }
+        [ValidateNever]
+        public int ProductId { get; set; }
 
         [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
@@ -28,5 +30,7 @@ namespace BKMeCommerce.Models
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
 
+        [NotMapped]
+        public decimal Price { get; set; }
     }
 }
---
+++ BKMeCommerceWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,121 @@
+﻿using BKMeCommerce.DataAccess.Repository.IRepository;
+using BKMeCommerce.Models;
+using BKMeCommerce.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BKMeCommerceWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartViewModel = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product"),
+
+            };
+
+            ShoppingCartViewModel.OrderTotal = 0;
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartViewModel.OrderTotal += cart.Price * cart.Count;
+            }
+
+            return View(ShoppingCartViewModel);
+        }
+
+        private decimal GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartViewModel = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product"),
+            };
+
+            return View(ShoppingCartViewModel);
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+            if (cart.Id == null || cart.Id == 0)
+            {
+                return BadRequest();
+            }
+            cart.Count++;
+            _unitOfWork.ShoppingCart.Update(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index)); 
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+            if (cart.Id == null || cart.Id == 0)
+            {
+                return BadRequest();
+            }
+            if (cart.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            }
+            else
+            {
+                cart.Count--;
+                _unitOfWork.ShoppingCart.Update(cart);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+            if (cart.Id == null || cart.Id == 0)
+            {
+                return BadRequest();
+            }
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
